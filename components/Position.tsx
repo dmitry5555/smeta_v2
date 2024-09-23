57c62baafde8954cade5ce1dc0a7dd8cdc3e0307
Dmitrii Larionov <mikrolux@gmail.com>
2024-09-23 20:08:55 +0700

    win fix

--- components/Position.tsx
+++ components/Position.tsx
@@ -128,13 +128,18 @@ const Position = ({ docKoefs, position, handlePosChange, uniqueId, toggleKoefsVi
 					step="any"
 					value={position.price}
 					onKeyDown={(event) => {
-						if (!/[0-9.]/.test(event.key) && 
-							event.key !== 'Backspace' && 
-							event.key !== 'Delete' && 
-							event.key !== 'ArrowLeft' && 
-							event.key !== 'ArrowRight' && 
-							event.key !== 'Tab') {
-						event.preventDefault();
+						const isNumeric = /[0-9.]/.test(event.key);
+						const isSpecialKey = 
+							event.key === 'Backspace' || 
+							event.key === 'Delete' || 
+							event.key === 'ArrowLeft' || 
+							event.key === 'ArrowRight' || 
+							event.key === 'Tab' || 
+							event.key === 'NumPadDecimal' || 
+							event.key === 'Period';
+					
+						if (!isNumeric && !isSpecialKey) {
+							event.preventDefault();
 						}
 					}}
 				/>
